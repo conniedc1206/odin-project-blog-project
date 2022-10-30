ab63edeb5de74a003dcab620cd471dfa0defd6de
Connie Park <conniedc1206@gmail.com>
2022-10-30 12:43:24 -0700

    Added root route, index action in Articles controller, and Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # a route that maps the root path of our application to the appropriate controller and action
+  root "articles#index"
+  
+  # The route declares that GET /articles requests are mapped to the index action of ArticlesController
+  get "/articles", to: "articles#index"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
