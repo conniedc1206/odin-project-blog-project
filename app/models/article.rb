494e2fb1eaa0a54754a4a825adfcaa07fd8cb19b
Connie Park <conniedc1206@gmail.com>
2022-10-30 13:18:06 -0700

    Completed Creating a New Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+# Rails provides a feature called validations to help us deal with invalid user input. Validations are rules that are checked before a model object is saved. 
+# If any of the checks fail, the save will be aborted, and appropriate error messages will be added to the errors attribute of the model object.
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # a route that maps the root path of our application to the appropriate controller and action
+  # Defines the root path route ("/")
   root "articles#index"
   
-  # The route declares that GET /articles requests are mapped to the index action of ArticlesController
-  get "/articles", to: "articles#index"
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # The route declares that GET /articles requests are mapped to the index action of ArticlesController
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
+  
 end
